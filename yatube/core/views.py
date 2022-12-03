596b91e4cc071301fd7463ac3f732c1f5f65b3c0
Vladimir Dmitriev <gdvsbook@gmail.com>
2022-12-03 10:26:08 +0300

    Исправлены замечания в приложении core

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -15,8 +15,10 @@ def csrf_failure(request, reason=''):
 
 
 def server_error(request):
-    return render(request, 'core/500.html', HTTPStatus.INTERNAL_SERVER_ERROR)
+    return render(
+        request, 'core/500.html', status=HTTPStatus.INTERNAL_SERVER_ERROR
+    )
 
 
 def permission_denied(request, exception):
-    return render(request, 'core/403.html', HTTPStatus.FORBIDDEN)
+    return render(request, 'core/403.html', status=HTTPStatus.FORBIDDEN)
